040dacb2514f92fc55027521a859ba602b9ae8d7
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2017-08-04 17:27:32 +0200

    :bear: establish some Reagent state for the table component

--- src/building_re_frame_components/sortable_table/student.cljs
+++ src/building_re_frame_components/sortable_table/student.cljs
@@ -60,21 +60,24 @@
    (get-in db [:tables key])))
 
 (defn sortable-table [table-key]
-  (let [table @(rf/subscribe [:table table-key])]
-     [:table {:style {:font-size "80%"}}
-      [:tr (for [h (:header table)]
-             [:th {:style {:line-height "1em"
-                           :padding-right "1em"}}
-              [:div {:style {:display :inline-block}} h]
-              [:div {:style {:display :inline-block
-                             :vertical-align :middle
-                             :font-size "80%"
-                             :margin-left "0.33333em"
-                             :line-height "1em"}}
-               [:div "▲"]
-               [:div "▼"]]])]
-      (for [row (:rows table)]
-        [:tr (for [v row] [:td v])])]))
+  (let [s (reagent/atom {})]
+    (fn [table-key]
+      (let [table @(rf/subscribe [:table table-key])]
+        [:div (pr-str @s)
+         [:table {:style {:font-size "80%"}}
+          [:tr (for [h (:header table)]
+                 [:th {:style {:line-height "1em"
+                               :padding-right "1em"}}
+                  [:div {:style {:display :inline-block}} h]
+                  [:div {:style {:display :inline-block
+                                 :vertical-align :middle
+                                 :font-size "80%"
+                                 :margin-left "0.33333em"
+                                 :line-height "1em"}}
+                   [:div "▲"]
+                   [:div "▼"]]])]
+          (for [row (:rows table)]
+            [:tr (for [v row] [:td v])])]]))))
 
 (defn ui []
   [:div
