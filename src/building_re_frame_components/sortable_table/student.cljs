e80f63887f4ded885bcc6e4bd3642e7e246938e0
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2017-08-04 16:54:51 +0200

    :bear: forgot that the subscribe should use the table-key value

--- src/building_re_frame_components/sortable_table/student.cljs
+++ src/building_re_frame_components/sortable_table/student.cljs
@@ -60,7 +60,7 @@
    (get-in db [:tables key])))
 
 (defn sortable-table [table-key]
-  (let [table @(rf/subscribe [:table :new-hope])]
+  (let [table @(rf/subscribe [:table table-key])]
      [:table {:style {:font-size "80%"}}
       [:tr (for [h (:header table)] [:th h])]
       (for [row (:rows table)]
