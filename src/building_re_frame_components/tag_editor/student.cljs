37c3f94c058d8ba86b8d5331404c02095aa711e9
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2019-02-14 18:20:57 +0100

    :bear: start out with an empty component
    201902141811 Tag Editor

--- src/building_re_frame_components/tag_editor/student.cljs
+++ src/building_re_frame_components/tag_editor/student.cljs
@@ -7,9 +7,10 @@
  (fn [db _]
    (select-keys db (filter #(= "teacher" (namespace %)) (keys db)))))
 
+(defn tag-editor [] [:div])
+
 (defn ui []
-  [:div
-   "Put tag editor here."])
+  [:div [tag-editor]])
 
 (when-some [el (js/document.getElementById "tag-editor--student")]
   (defonce _init (rf/dispatch-sync [:initialize]))
